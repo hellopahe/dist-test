e949b4a227438147bdd593e1c80c87c54a1cd764
tim <hellopanghe@gmail.com>
2023-02-08 08:37:38 +0800

    init

---
+++ dist/precache-manifest.39ba1da9aa52dabcae1be92e47ff45dc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d778f6dcaa9efe24e9ee",
+    "url": "/css/app.ec4b43ad.css"
+  },
+  {
+    "revision": "0d8efb0b836d0212c4cf",
+    "url": "/css/chunk-vendors.c13088a3.css"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/fonts/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "abe71f7d608d43b56d9b2aef78d7ae99",
+    "url": "/fonts/element-icons.abe71f7d.ttf"
+  },
+  {
+    "revision": "d9491be2c5109fca0fa40d0c59e2e3b9",
+    "url": "/fonts/element-icons.d9491be2.woff"
+  },
+  {
+    "revision": "359cdbae0effb4fc9a8ef6dab11dfbe1",
+    "url": "/fonts/iconfont.359cdbae.ttf"
+  },
+  {
+    "revision": "76281ca263db8df044e747190116c78e",
+    "url": "/fonts/iconfont.76281ca2.woff2"
+  },
+  {
+    "revision": "f5c913de14050afbba48e5da78e2dadf",
+    "url": "/fonts/iconfont.f5c913de.woff"
+  },
+  {
+    "revision": "105ab9f41e913ec693ed728acf3c2c61",
+    "url": "/fonts/kust.woff.105ab9f4.ttf"
+  },
+  {
+    "revision": "3b825964d4147dc42bfda8d0b658d10a",
+    "url": "/img/connect.3b825964.jpg"
+  },
+  {
+    "revision": "2c7e3be033812cd686ba0cd3a827578c",
+    "url": "/img/index_bg.2c7e3be0.png"
+  },
+  {
+    "revision": "92b1bf878c10466bee86032ff060b583",
+    "url": "/img/logo.92b1bf87.jpg"
+  },
+  {
+    "revision": "24bfca442a4780aafce8fe63dca89f58",
+    "url": "/img/metamask.24bfca44.png"
+  },
+  {
+    "revision": "8c726bcb5ff682d445da193f4ccfaa64",
+    "url": "/img/nocontent.8c726bcb.png"
+  },
+  {
+    "revision": "87906ecfef8cbea2e772485e1c569788",
+    "url": "/img/non-existent.87906ecf.png"
+  },
+  {
+    "revision": "96a05f076444063d258827b3a68a9256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d778f6dcaa9efe24e9ee",
+    "url": "/js/app.21c74992.js"
+  },
+  {
+    "revision": "0d8efb0b836d0212c4cf",
+    "url": "/js/chunk-vendors.3f9a777a.js"
+  },
+  {
+    "revision": "f0be909bd1db7239439e7b324393471f",
+    "url": "/manifest.json"
+  }
+]);
